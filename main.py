72d2b6f70f75da2386d0f8261c6fdd0ed4f380ec
Data-Mark 4 <1024935841@qq.com>
2022-05-02 11:14:10 +0800

    新增log记录

--- main.py
+++ main.py
@@ -35,8 +35,10 @@ def main(args):
     env.unwrapped
     agent = PPO(state_dim=8, action_dim=2, batch_size=16)
 
-    # log 初始化
-    # log = log2json()
+    # Iter log初始化
+    logger_iter = log2json(filename='train_log_iter', type_json=True)
+    # epoch log初始化
+    logger_ep = log2json(filename='train_log_ep', type_json=True)
 
     # 预训练？
     if args.pre_train:
@@ -45,10 +47,12 @@ def main(args):
             agent.load_model(checkpoint)
 
     ep_history = []
+
     epochs = tqdm(range(args.epochs), leave=False, position=0, colour='green')
 
     for epoch in epochs:
         reward_history = 0
+        entropy_history = 0
         obs = env.reset()
         step = tqdm(range(args.max_timestep*5), leave=False, position=1, colour='red')
         for t in step:
@@ -69,23 +73,49 @@ def main(args):
                     discount_reward = agent.decayed_reward(obs_t1, reward_nstep)
                     agent.update(state, action, logprob_nstep, discount_reward)
                     agent.memory.clear()
+            entropy = dist.entropy().numpy().sum().item()
+            log_text = {'epochs': epoch,
+                        'time_step': agent.t,
+                        'reward': reward,
+                        'entropy': entropy,
+                        'acc': act[0].item(),
+                        'ori': act[1].item(),
+                        'actor_loss': agent.history_actor,
+                        'critic_loss': agent.history_critic}
             step.set_description(f'epochs:{epoch}, '
                                  f'time_step:{agent.t}, '
                                  f'reward:{reward:.1f}, '
-                                 f'entropy: {dist.entropy().numpy().sum().item():.1f}, '
-                                 f'acc:{act[0].item():.1f}, '
-                                 f'ori:{act[1].item():.1f}')
-            if done:
-                break
+                                 f'entropy: {log_text["entropy"]:.1f}, '
+                                 f'acc:{log_text["acc"]:.1f}, '
+                                 f'ori:{log_text["ori"]:.1f}, '
+                                 f'actor_loss:{agent.history_actor:.1f}, '
+                                 f'critic_loss:{agent.history_critic:.1f}')
+            # iter数据写入log文件
+            logger_iter.write2json(log_text)
+
             # 记录timestep, reward＿sum
             agent.t += 1
             obs = obs_t1
             reward_history += reward
+            entropy_history += entropy
+
+            if done:
+                break
 
             if t + 1 % (args.max_timestep*5) == 0:
                 break
 
         ep_history.append(reward_history)
+        log_ep_text = {'epochs': epoch,
+                       'time_step': agent.t,
+                       'ep_reward': reward_history,
+                       'entropy_mean': entropy_history / (t+1)}
+        epochs.set_description(f'epochs:{epoch}, '
+                               f'time_step:{agent.t}, '
+                               f'reward:{reward_history:.1f}, '
+                               f'entropy:{log_ep_text["entropy_mean"]:.1f}')
+        # epoch数据写入log文件
+        logger_ep.write2json(log_ep_text)
     env.close()
 
 
